1c695c5512aefdb532573641a988ce34211ed9ff
Ismael <deyris@gmail.com>
2021-01-04 06:36:28 +0100

    Fixing navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ArtistsComponent } from './components/artists/artists.component';
+import { WorksComponent } from './components/works/works.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'heroes', component: ArtistsComponent },
-  
+    { path: 'artists', component: ArtistsComponent },
+    { path: 'works', component: WorksComponent },
+
     { path: '**', pathMatch:'full', redirectTo: 'home' }
 ];
 
